238e8c587327821811f566b12a58eb2aeb9e9d1d
Marilyn Craft <craft.fleur@gmail.com>
2021-07-02 18:54:31 -0400

    ch6-prac04 update01

--- ch6_dictionaries-prac04.py
+++ ch6_dictionaries-prac04.py
@@ -5,8 +5,22 @@ user_1 = {
     'first': 'Brittany',
     'last': 'Del Bryant'
 }
+
 # using two variables to store the key and value pairs; and also the keyword '.items()'
 for key, value in user_1.items():
     print(f"\nKey: {key}")
     print(f"Value: {value}")
 
+# using the dictionary from before
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+}
+for n, l in favorite_languages.items():
+    print(f"\n{n.title()}'s favorite language is {l.title()}.")
+
+# if we just wanted the names (keys) for the people in our dictionary, we would use the keyword '.keys()'
+for k in favorite_languages.keys():
+    print(f"{k.title()}")
