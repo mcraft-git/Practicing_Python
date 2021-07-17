8f343f1fc931f117a84e7d6523a8e178d1595e69
Marilyn Craft <craft.fleur@gmail.com>
2021-07-17 14:53:39 -0400

    cbt_api_1

--- sample_yaml.py
+++ sample_yaml.py
@@ -1,4 +1,4 @@
-# This is a Python file that will stream our YAML document to display on the terminal
+# This is a Python file that will parse our YAML document to display on the terminal
 # First run command "pip3 install pyyaml" from the terminal
 
 import yaml
@@ -7,9 +7,21 @@ from yaml import load, load_all
 stream = open('sample_yaml.yml','r')
 data = load_all(stream, Loader=yaml.FullLoader)
 
+# Below is a more complex parse
+
+#for doc in data:
+#    print("New Document:")
+#    for key, value in doc.items():
+#        print(key + ": " + str(value))
+#        if type(value) is list:
+#           print(str(len(value)))
+
+# Here is a simpler parse
+print("")
 for doc in data:
-    print("New Document:")
-    for key, value in doc.items():
-        print(key + ": " + str(value))
-        if type(value) is list:
-            print(str(len(value)))
+    print(type(doc))
+    if type(doc) is dict:
+        try:
+            print(doc["people"][2])
+        except:
+            print(doc)
