a0b0da6349673613420a86230d7cdca255224e21
mcraft <craft.fleur@gmail.com>
2021-08-21 16:25:09 -0400

    mod-last-entry

--- ch7_inputs_anime-poll.py
+++ ch7_inputs_anime-poll.py
@@ -1,10 +1,11 @@
 
-
+# Let's practice inputs with dictionaries and loops!
 responses = {}
-name_bank = []
 polling_active = True
 
 print("\n~ ~ ~ Welcome to the Mandatory Anime Poll! ~ ~ ~")
+
+# Loop will continue while 'polling active' is 'True'
 while polling_active:
 
     name = input("\nWhat is your name?\n")
@@ -12,21 +13,16 @@ while polling_active:
     anime = input(f"\nHello, {name.title()}. What is the best anime?\n")
     anime = anime.lower()
 
+    # Save each input response as a dictionary key(name) + value(anime)
     responses[name] = anime
 
     repeat = input("\nThank you. Is there anyone else who needs to answer? (yes/no)\n")
 
+    # Exit loop if input is "no"
     if repeat.lower() == "no":
         polling_active = False
 
-for name in responses:
-
-    if name in name_bank:
-        print(f"\nNice try, {name.title()}. There can be only one BEST ANIME.")
-        responses.pop(name)
-    else:
-        name_bank.append(name)
-
+# Check 'responses' dictionary items against if statements
 for name, anime in responses.items():
     print(f"\n{name.title()}'s favorite anime is {anime.title()}.")
 
