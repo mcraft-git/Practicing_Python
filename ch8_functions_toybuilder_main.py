952e62f6de5f5523d372b0871663e41f9cd84cfa
Marilyn Craft <craft.fleur@gmail.com>
2021-08-28 20:42:08 -0400

    ch8_toybuilder_1

--- ch8_functions_toybuilder_main.py
+++ ch8_functions_toybuilder_main.py
@@ -1,5 +1,16 @@
 
 # Let's use functions to make a toybuilder program!
 
-# When importing, shorten the name to make calls easier
+# When importing, shorten the name to make calls easier.
 import functions_toybuilder_module as tm
+
+print("A new toy is waiting to be born and be your new best friend!\n")
+print("But it needs your help figuring out what sort of toy it will be...\n")
+
+toy_type = input("What type of toy is it? (pick a number)"
+"\n\t1. Teddy Bear\n\t2. Dress-up Dolly\n\t3. Magic Unicorn\n\t4. Rowdy Robot")
+
+toy_parts_fin = tm.toy_parts(toy_type)
+
+toy_name = input("What is its name?\n")
+
--- ch8_functions_toybuilder_module.py
+++ ch8_functions_toybuilder_module.py
@@ -2,9 +2,20 @@
 # This is a module file. Function definitions are saved in modules.
 # The main file of a program will import functions from modules.
 
-# 
+
 # Notice the double asterisk before the "parts" parameter,
 # which indicates an arbitrary number of Keyword Arguments
 # may be passed to the function as 'key=value' pairs.
 def toy_specifications(type,name,**parts):
-    """Builds profile of desired toy. Prints profile."""
+    """Builds profile of desired toy. Prints build status."""
+
+
+
+def toy_parts(type):
+    """Prints questionaire to obtain inputs.
+        Compiles list of desired parts."""
+chosen_type = type
+
+
+def toy_complete(spec):
+    """Prints completed toy profile."""
