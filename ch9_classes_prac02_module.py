ede43daf02bc12c071fb715ca314d1b658bb7268
Marilyn Craft <craft.fleur@gmail.com>
2021-09-02 18:06:00 -0400

    mod

--- ch9_classes_prac02_module.py
+++ ch9_classes_prac02_module.py
@@ -46,18 +46,3 @@ class User:
             print("zhjsbcfjkfdc\n"
             "Okay, you got us. Failing the reset is pretty funny.")
 
-print("So... you think you're pretty funny?\n"
-"Then why not join our super exclusive, super hilarious community of comedic narcissists?\n")
-
-username = input("Enter a username: ")
-joined = input("Enter today's date (mm/dd/yyyy: ")
-password = input ("Enter a password: ")
-
-user1 = User(username,joined,password)
-
-print(f"Here are your details:\n"
-"\tusername: {user1.username}\n"
-"\tjoined: {user1.joined}\n"
-"\tpassword: {user1.password}")
-
-input 
