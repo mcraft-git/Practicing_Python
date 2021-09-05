e2bdd8d4b38d2217de0d48b70f683cf84b3e789d
Marilyn Craft <craft.fleur@gmail.com>
2021-09-04 20:10:24 -0400

    ch10_files_prac01

---
+++ ch10_files_prac01.py
@@ -0,0 +1,30 @@
+
+# Let's work with some files!
+# The keyword 'with' closes the file when the program is finished,
+# which we always want to do (otherwise corruption or loss may occur).
+# Another way would be with 'close()'.
+with open('pi_digits.txt') as file_object:
+    contents = file_object.read()
+# Including the 'rstrip()' method removes the blank line left by 'read()'.    
+print(contents.rstrip())
+
+print("")
+# This version looks for the file "hello binary.txt"
+# in a subfolder of the current directory.
+with open('Crash_Course_2/hello_binary.txt') as file_object:
+    contents = file_object.read()
+    print(contents.rstrip())
+
+# We can also use the full file path, however backslashes
+# used in Windows directories must be doubled (see example below).
+# This is because a single backslash is used to escape characters.
+
+# Saving a path as a variable is common practice.
+file_path = 'C:\\Users\\craft\\00_codez\\Python\\Crash_Course\\Crash_Course_2\\buncha_lines.txt'
+
+with open(file_path) as file_object:
+    # We can use a loop to parse each line,
+    # which is great for searching a file.
+    for line in file_object:
+        # Don't forget to strip!
+        print(line.rstrip())
