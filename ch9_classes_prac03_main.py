41665ce85830b89cae9e6e8cfc32b847d58fe0dd
Marilyn Craft <craft.fleur@gmail.com>
2022-01-14 16:53:02 -0500

    added standalone statement

--- ch9_classes_prac03_main.py
+++ ch9_classes_prac03_main.py
@@ -1,9 +1,14 @@
 
-"""Main program."""
+# This program builds upon the last practice project.
+# We will create a login system for a fake web message board.
+# It will not be secure (but we are just practicing with classes).
+# It will not write to any files -- that's next chapter!
 
-# This program will first import a module.
+# We will first import one of our modules.
 import ch9_classes_prac03_module3 as mod3
 
 # Then call a function to get the ball rolling...
 # (See module3 comments)
-mod3.create_account()
+if __name__ == '__main__':
+
+    mod3.create_account()
